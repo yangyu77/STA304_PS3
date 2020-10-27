98f0b8cd00b4a083116263705aaa2c14f6f4107c
Yang <60946346+yangyu77@users.noreply.github.com>
2020-10-26 22:46:53 -0400

    Add files via upload

---
+++ 01-data_cleaning-survey1.R
@@ -0,0 +1,56 @@
+#### Preamble ####
+# Purpose: Prepare and clean the survey data downloaded from [...UPDATE ME!!!!!]
+# Author: Rohan Alexander and Sam Caetano [CHANGE THIS TO YOUR NAME!!!!]
+# Data: 22 October 2020
+# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the data from X and save the folder that you're 
+# interested in to inputs/data 
+# - Don't forget to gitignore it!
+
+
+#### Workspace setup ####
+library(haven)
+library(tidyverse)
+setwd("~/Downloads/UofT 2018-2022/Academic/STA304/STA304_PS3")
+# Read in the raw data (You might need to change this if you use a different dataset)
+raw_data <- read_dta("ns20200625.dta")
+# Add the labels
+raw_data <- labelled::to_factor(raw_data)
+# Just keep some variables
+reduced_data <- 
+  raw_data %>% 
+  select(interest,
+         registration,
+         vote_2016,
+         vote_intention,
+         vote_2020,
+         ideo5,
+         employment,
+         foreign_born,
+         gender,
+         census_region,
+         hispanic,
+         race_ethnicity,
+         household_income,
+         education,
+         state,
+         congress_district,
+         age)
+
+
+#### What else???? ####
+# Maybe make some age-groups?
+# Maybe check the values?
+# Is vote a binary? If not, what are you going to do?
+
+reduced_data<-
+  reduced_data %>%
+  mutate(vote_trump = 
+           ifelse(vote_2020=="Donald Trump", 1, 0))
+
+# Saving the survey/sample data as a csv file in my
+# working directory
+write_csv(reduced_data, "/Users/yangyuxin/Downloads/survey_data.csv")
+
